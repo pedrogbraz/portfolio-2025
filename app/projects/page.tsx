6d70c7b983d3e858e9cd5eefa95c5345162c74c1
pedrogbraz <pedrobrazdevs@gmail.com>
2025-04-24 16:17:42 -0300

    feat: add initial my projects page

---
+++ app/projects/page.tsx
@@ -0,0 +1,14 @@
+export default function Projects() {
+  return (
+    <>
+      <section className="px-4 py-[18px]">
+        <div className="flex flex-col xxs:flex-row gap-2 justify-between">
+          <div className="flex items-center gap-2">
+            <div className="bg-zinc-400 dark:bg-zinc-600 size-1.5 rounded-full" />
+            <h1 className="font-medium text-muted-foreground">Meus Projetos</h1>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
