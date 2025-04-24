1472b6bf531cb5975644a4f693f3f3b6ccc9c631
pedrogbraz <pedrobrazdevs@gmail.com>
2025-04-24 20:19:04 -0300

    feat: add new informations to my-projects page

--- components/sections/projects/my-projects.tsx
+++ components/sections/projects/my-projects.tsx
@@ -1,10 +1,14 @@
 "use client";
 
+import Link from "next/link";
 import { useEffect, useState } from "react";
 
 interface GitHubRepo {
   name: string;
   html_url: string;
+  language: string;
+  visibility: string;
+  created_at: string;
 }
 
 export function MyProjects() {
@@ -26,19 +30,42 @@ export function MyProjects() {
     fetchRepos();
   }, []);
 
+  function formatDate(dateString: string) {
+    const date = new Date(dateString);
+    return date.toLocaleDateString("pt-BR", {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+    });
+  }
+
   return (
     <div>
-      <ul className="space-y-2">
+      <ul className="space-y-2 grid sm:grid-cols-2 gap-2">
         {repos.map((repo) => (
-          <li key={repo.html_url}>
-            <a
-              href={repo.html_url}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-500 hover:underline"
-            >
-              {repo.name}
-            </a>
+          <li
+            key={repo.html_url}
+            className="bg-[#FFFFFF] dark:bg-[#212121] py-2.5 px-4 rounded-lg w-full flex flex-col gap-4"
+          >
+            <div className="flex items-center justify-between">
+              <Link
+                href={repo.html_url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-500 hover:underline text-nowrap text-ellipsis overflow-x-hidden max-w-[70%]"
+              >
+                {repo.name}
+              </Link>
+              <span className="text-xs text-muted-foreground">
+                {repo.visibility}
+              </span>
+            </div>
+            <div className="flex items-center justify-between">
+              <h2 className="text-xs">{repo.language}</h2>
+              <h2 className="text-xs text-muted-foreground">
+                {formatDate(repo.created_at)}
+              </h2>
+            </div>
           </li>
         ))}
       </ul>
